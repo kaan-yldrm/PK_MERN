cb5466394a2d211605d2f073ccec60fa86c82f25
Kaan kripto <kaan_yldrm@live.com>
2023-02-12 15:15:49 +0300

    Baby Steps

---
+++ notes2.js
@@ -0,0 +1,4 @@
+let outpt = require('./notes1')
+
+console.log(outpt)          // notes1.js dosyasının içindeki nesneyi ve içeriğini çeker
+console.log(outpt.firms)    // notes1.js dosyasının içindeki firms nesnesinin içeriğini çeker
