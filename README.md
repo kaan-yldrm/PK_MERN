7e5d00948e69fbf787584440256fe86ed31ae68b
Kaan YILDIRIM <kaan_yldrm@live.com>
2023-02-13 17:26:34 +0300

    installed morgan, express, nodemon vid2 is done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This webapp is to be used for inspection report logs, store and use the data to fill template files and create .pdf files for customers. (Hopefully)
+
+express
+morgan
+nodemon
---
+++ vid2/server.js
@@ -0,0 +1,48 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+    res.setHeader('Content-Type', 'text/html')
+    /* res.write('<h1><title>PK_MERN_STACK</title></h1>')  // sekme adına Title yazar
+    res.write('<h1>merhaba</h1>')                       // sayfa içine Header yazar
+    res.end() */
+
+
+    let path ='./vid2/'
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            break
+
+        case '/about':
+            path += 'about.html'
+            break
+        
+        case '/about-us':
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            break
+
+        default:
+            path += '404.html'
+            res.statusCode = 404
+            break
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end()
+        } else {
+            res.write(data);
+            res.end()
+        }
+    })
+})
+
+const port = process.env.port || 3000;
+
+server.listen(port, () => {
+    console.log("Online")
+})
